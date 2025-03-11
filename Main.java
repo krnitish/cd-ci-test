cb9bf6d98b410fd9f230dfeaf3c5676915dfc07e
nitish4-kumar <nitish4.kumar@outlook.com>
2025-03-11 15:25:17 +0530

    2nd commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main{
         System.out.println("this is java program");
         System.out.println("output from java program");
         System.out.println(new Date());
+        System.out.println("Running from github update 1st time.")
     }
 }
